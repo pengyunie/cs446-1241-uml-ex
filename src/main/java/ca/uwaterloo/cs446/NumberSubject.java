3ad22788a0df6ac4bc3694c7d70c4390026fbfe0
Monir Nasserie <58013715+MN-Coding@users.noreply.github.com>
2024-01-22 11:43:58 -0500

    fixed

--- src/main/java/ca/uwaterloo/cs446/NumberSubject.java
+++ src/main/java/ca/uwaterloo/cs446/NumberSubject.java
@@ -1,9 +1,20 @@
 package ca.uwaterloo.cs446;
 
 public class NumberSubject extends Subject {
+
+    private int state;
+
     public NumberSubject(int state) {
         super();
         this.setState(state);
+    }
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
         sendNotice();
     }
 }
--- src/main/java/ca/uwaterloo/cs446/Subject.java
+++ src/main/java/ca/uwaterloo/cs446/Subject.java
@@ -2,9 +2,8 @@ package ca.uwaterloo.cs446;
 
 import java.util.ArrayList;
 
-public class Subject {
+public abstract class Subject {
     private ArrayList<Observer> observers;
-    private int state;
 
     public Subject() {
         observers = new ArrayList<>();
@@ -18,12 +17,8 @@ public class Subject {
         observers.forEach(o -> o.update());
     }
 
-    public int getState() {
-        return state;
-    }
+    public abstract int getState();
 
-    public void setState(int s) {
-        state = s;
-    }
+    public abstract void setState(int s);
 
 }
