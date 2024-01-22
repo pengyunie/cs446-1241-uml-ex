63533fc1f61e7bca63e36df77c6e7799b6940f61
Harry <harryhe187@gmail.com>
2024-01-22 09:42:19 -0500

    .

--- src/main/java/ca/uwaterloo/cs446/Observer.java
+++ src/main/java/ca/uwaterloo/cs446/Observer.java
@@ -1,7 +1,7 @@
 package ca.uwaterloo.cs446;
 
 public abstract class Observer {
-    private Subject subject = null;
+    protected Subject subject = null;
     public abstract void update();
     public abstract String getValue();
 }
