20dbf6e7396d6f5ebb83d8d37be5edc78e4a3b1c
Katherine Mu <katherinemu47@gmail.com>
2024-01-22 09:00:42 -0500

    fix OctalObserver

--- src/main/java/ca/uwaterloo/cs446/OctalObserver.java
+++ src/main/java/ca/uwaterloo/cs446/OctalObserver.java
@@ -2,14 +2,15 @@ package ca.uwaterloo.cs446;
 
 public class OctalObserver extends Observer{
     private String octValue;
+    private Subject subject;
 
     public OctalObserver(subject: Subject) {
-        subject.attach(self)
+        this.subject = subject;
+        subject.attach(this)
     }
 
     public void update() {
-        Integer.toOctalString(octValue);
-        subject.getState();
+        octValue = Integer.toOctalString(subject.getState());
     }
     public String getValue() {
         return this.octValue;
